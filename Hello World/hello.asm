05f852a2a23855ed3d8023cdacb402d95f26e347
Zachary Nicholas <115509575+Toast-stack@users.noreply.github.com>
2025-07-18 01:38:07 -0400

    Add files via upload

---
+++ Hello World/hello.asm
@@ -0,0 +1,38 @@
+	.file	"hello.cpp"
+	.intel_syntax noprefix
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello World!\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2050:
+	push	rbp
+	.seh_pushreg	rbp
+	mov	rbp, rsp
+	.seh_setframe	rbp, 0
+	sub	rsp, 32
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	lea	rdx, .LC0[rip]
+	mov	rax, QWORD PTR .refptr._ZSt4cout[rip]
+	mov	rcx, rax
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	mov	eax, 0
+	add	rsp, 32
+	pop	rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (GNU) 15.1.0"
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.p2align	3, 0
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
